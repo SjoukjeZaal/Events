b523415bb0c61d08b90a165f9d865ecaf8e575f2
Sjoukje Zaal <szaal@outlook.com>
2019-02-26 09:45:16 +0100

    Added

---
+++ 2018-12-09 Dear Azure/invitation.ps1
@@ -0,0 +1,16 @@
+﻿$cred = Get-Credential
+$tenantId = "<your-tenantId-here>" 
+
+Connect-AzureAD -Credential $cred -TenantId $tenantId
+
+$invitations = import-csv "C:\...\invitations.csv"
+$messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
+$messageInfo.customizedMessageBody = "Hey there! Check this out. I created an invitation through PowerShell"
+
+foreach ($email in $invitations) {
+    New-AzureADMSInvitation -InvitedUserEmailAddress $email.InvitedUserEmailAddress `
+                            -InvitedUserDisplayName $email.Name `
+                            -InviteRedirectUrl https://myapps.microsoft.com `
+                            -InvitedUserMessageInfo $messageInfo `
+                            -SendInvitationMessage $true
+                        }
